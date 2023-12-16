d229e1d34fe06189d005c115c287ab8e23416219
HuangWei <market@zuiyue.com>
2023-12-16 11:00:46 +0800

    更新测试

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,27 @@ async fn main() {
 }
 
 
-// #[tokio::main]
-// async fn main() {
-//     println!("{}", wei_hardware::get_ip_info().await);
+
+// use os_info::{Type};
+
+// fn main() -> std::io::Result<()> {
+//     let info = os_info::get();
+
+//     if info.os_type() == Type::Windows {
+//         // 将版本号转换为字符串并分割
+//         let version_str = info.version().to_string();
+//         let parts: Vec<&str> = version_str.split('.').collect();
+
+//         let version = parts[0].parse::<u32>().unwrap();
+
+//         if version <= 6 {
+//             println!("Windows 7");
+//         } else if version == 10 {
+//             println!("Windows 10");
+//         } else {
+//             println!("Windows {}", parts[0]);
+//         }
+//     }
+
+//     Ok(())
 // }
