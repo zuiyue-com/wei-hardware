7cfb880d87fd5c8faa3be5d639d898cd5e610882
HuangWei <market@zuiyue.com>
2024-01-16 13:32:50 +0800

    修改：替换物料文件

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #[cfg(target_os = "windows")]
-static DATA_1: &'static [u8] = include_bytes!("../../wei-release/windows/qbittorrent/qbittorrent.exe");
+static DATA_1: &'static [u8] = include_bytes!("../../wei-release/windows/san/san.txt");
 
 use serde_json::{json};
 
